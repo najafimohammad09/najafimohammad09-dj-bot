6830c4791ca8a15a8b987cdc595bf178f3291b70
najafimohammad09 <najafimohammad00009@gmail.com>
2016-03-19 07:16:00 +0100

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = { 112707993,195129537,185963103,198488095, }  ,--Sudo users
+    sudo_users = { ,112707993,195129537,185963103,208928649,198488095, }  ,--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[W_SaTaN_W V 1
